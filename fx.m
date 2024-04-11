515458015e1659bc3bd492bc665f8ed05a5e274c
Alone-together-Fire <2022222030@email.szu.edu.cn>
2024-04-11 23:12:55 +0800

    更新实验三代码的所有注释

--- fx.m
+++ fx.m
@@ -1,10 +1,9 @@
-function [outputArg1] = fx(ix)
+function [outputArg1] = fx(ix)%输入与返回都只有一个参数
 %UNTITLED 此处显示有关此函数的摘要
 %   此处显示详细说明
-fenmu1 = 0.1 + (ix - 2) * (ix - 2);
-fenmu2 = 0.01 + (ix - 3) * (ix - 3) * (ix - 3) * (ix - 3);
-format rat
-outputArg1 = (1 ./ fenmu1) + (1 ./ fenmu2);
-
+fenmu1 = 0.1 + (ix - 2) * (ix - 2);%计算第一个项的分母
+fenmu2 = 0.01 + (ix - 3) * (ix - 3) * (ix - 3) * (ix - 3);%计算第二个项的分母
+format rat%答案以分数形式显示
+outputArg1 = (1 ./ fenmu1) + (1 ./ fenmu2);%计算函数的最终答案,作为返回值
 end
 
--- homework0411.m
+++ homework0411.m
@@ -53,69 +53,70 @@
 % n = str2double(userInput);
 % disp(['输入的n为: ' num2str(n)]);
 % 
-% fenzi0 = 4
-% for i = 4:2:n*2
-%     fenzi = fenzi0 * i * i
-%     fenzi0 = fenzi
+% %计算方法:先算出总的分子与分母,再将它俩相除得到最后的分数
+% fenzi0 = 4;%起始时2*2=4
+% for i = 4:2:n*2%i从4开始,每次+2,最大为2n
+%     fenzi = fenzi0 * i * i;
+%     fenzi0 = fenzi;
 %     %disp('此时=')
 %     %disp(fenzi)
 % end
-% disp('分子=')
+% disp('最终分子=')
 % disp(fenzi)
 % 
-% fenmu0 = 3
-% for j1 = 3:2:(n*2-1)
-%     j2 = j1 + 2
-%     fenmu = fenmu0 * j1 * j2
-%     fenmu0 = fenmu
+% fenmu0 = 3;%起始时1*3=3
+% for j1 = 3:2:(n*2-1)%j1从3开始,每次+2,最大为2n-1
+%     j2 = j1 + 2;
+%     fenmu = fenmu0 * j1 * j2;
+%     fenmu0 = fenmu;
 %     %disp('此时=')
 %     %disp(fenmu)
 % end
-% disp('分母=')
+% disp('最终分母=')
 % disp(fenmu)
 % 
-% format rat
-% ans = fenzi / fenmu
-% disp('答案=')
-% disp(ans)
+% format rat%最终答案以分数形式显示
+% answ = fenzi / fenmu;
+% disp('最终答案=')
+% disp(answ)
 
 %第二题-向量运算
 % userInput = input('请输入n的值: ', 's'); 
 % n = str2double(userInput);
 % disp(['输入的n为: ' num2str(n)]);
 % 
-% Afenzi = [2:2:(n * 2)]
+% %计算方法:先算出总的分子与分母,再将它俩相除得到最后的分数;总的分子先拆成两个矩阵,各自用prod算出乘积,再把各自的乘积相乘,即为总的
+% Afenzi = 2:2:(n * 2);%构建分子的矩阵;只有一列,从2开始,每次+2,最大为2n
 % %disp('Afenzi=')
 % %disp(Afenzi)
-% fenzi0 = prod(Afenzi)
-% fenzi = fenzi0 * fenzi0
-% disp('fenzi=')
+% fenzi0 = prod(Afenzi);%得到一半的乘积
+% fenzi = fenzi0 * fenzi0;%得到总的分子
+% disp('最终分子=')
 % disp(fenzi)
 % 
-% Bfenmu1 = [1:2:(n * 2 - 1)]
-% Bfenmu2 = [1:2:(n * 2 + 1)]
+% Bfenmu1 = 1:2:(n * 2 - 1);%构建分母的一半矩阵;只有一列,从1开始,每次+2,最大为2n-1
+% Bfenmu2 = 1:2:(n * 2 + 1);%构建分母的一半矩阵;只有一列,从1开始,每次+2,最大为2n+1
 % %disp('Bfenmu=')
 % %disp(Bfenmu)
-% fenmu1 = prod(Bfenmu1)
-% fenmu2 = prod(Bfenmu2)
-% fenmu = fenmu1 * fenmu2
-% disp('fenmu=')
+% fenmu1 = prod(Bfenmu1);%得到一半的乘积
+% fenmu2 = prod(Bfenmu2);%得到一半的乘积
+% fenmu = fenmu1 * fenmu2;%得到总的分母
+% disp('最终分母=')
 % disp(fenmu)
 % 
-% format rat
-% ans = fenzi / fenmu
-% disp('答案=')
-% disp(ans)
+% format rat%最终答案以分数形式显示
+% answ = fenzi / fenmu;
+% disp('最终答案=')
+% disp(answ)
 
 %第三题
-%A = [2, 43, 24; 8, 4, 21; 45, 6, 8];
-rows = input('请输入矩阵的行数：');
-cols = input('请输入矩阵的列数：');
-A = zeros(rows, cols); % 创建一个初始元素为零的矩阵
-for i = 1:rows
-    for j = 1:cols
-        A(i, j) = input(['请输入矩阵元素(' num2str(i) ',' num2str(j) '): ']);
-    end
-end
-B = fx(A);
-disp(B)
+% rows = input('请输入矩阵的行数：');
+% cols = input('请输入矩阵的列数：');
+% A = zeros(rows, cols); % 创建一个初始元素为零的矩阵
+% for i = 1:rows
+%     for j = 1:cols
+%         A(i, j) = input(['请输入矩阵元素(' num2str(i) ',' num2str(j) '): ']);
+%     end
+% end
+% B = fx(A);%调用fx函数
+% disp(B)%显示答案
